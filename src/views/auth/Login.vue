8589e18d0638350f6076db213cb525e526d0e3ba
Gints Polis <polis.gints@gmail.com>
2023-02-18 16:47:02 +0200

    updated login form

--- src/views/auth/Login.vue
+++ src/views/auth/Login.vue
@@ -1,61 +1,59 @@
 <template>
-  <div class="d-flex align-center justify-center" style="height: 100vh">
-      <v-sheet width="400" class="mx-auto">
-          <v-form fast-fail @submit.prevent="login">
-              <v-text-field v-model="username" label="User Name"></v-text-field>
+  <v-form fast-fail @submit.prevent="login">
+    <v-text-field type="email" v-model="username" label="Email"></v-text-field>
+    <v-text-field v-model="password" label="password" :append-icon="show ? 'mdi-eye' : 'mdi-eye-off'"
+      :type="show ? 'text' : 'password'" @click:append="show = !show"></v-text-field>
+    <RouterLink to="forgot">Forgot Password?</RouterLink>
 
-              <v-text-field type="password" v-model="password" label="password"></v-text-field>
-              <a href="#" class="text-body-2 font-weight-regular">Forgot Password?</a>
+    <v-btn type="submit" color="primary" block class="mt-2">Sign in</v-btn>
 
-              <v-btn type="submit" color="primary" block class="mt-2">Sign in</v-btn>
-
-          </v-form>
-          <div class="mt-2">
-              <p class="text-body-2">Don't have an account? <RouterLink to="/auth/register">Sign Up</RouterLink></p>
-          </div>
-      </v-sheet>
+  </v-form>
+  <div class="mt-2">
+    <p class="text-body-2">Don't have an account? <RouterLink to="/auth/register">Sign Up</RouterLink>
+    </p>
   </div>
 </template>
 
 <script lang="ts" setup>
-  import { CognitoUserPool, AuthenticationDetails, CognitoUser } from "amazon-cognito-identity-js"
-  import { ref } from 'vue'
-  import { useRouter } from 'vue-router'
-  import { useTokenStore } from "@/store/auth/tokenStore";
-  import poolData from "./poolData"
+import { CognitoUserPool, AuthenticationDetails, CognitoUser } from "amazon-cognito-identity-js"
+import { ref } from 'vue'
+import { useRouter } from 'vue-router'
+import { useTokenStore } from "@/store/auth/tokenStore";
+import poolData from "./poolData"
 
-  const router = useRouter()
-  const tokenStore = useTokenStore()
+const router = useRouter()
+const tokenStore = useTokenStore()
 
-  const username = ref('')
-  const password = ref('')
+const show = ref(false)
+const username = ref('')
+const password = ref('')
 
-  const login = () => {
-    const authenticationData = {
-      Username: username.value,
-      Password: password.value
-    }
-
-    let authenticationDetails = new AuthenticationDetails(authenticationData)
-
-    const userPool = new CognitoUserPool(poolData)
-
-    const userData = {
-      Username: username.value,
-      Pool: userPool
-    }
-
-    const cognitoUser = new CognitoUser(userData)
-
-    cognitoUser.authenticateUser(authenticationDetails, {
-      onSuccess: (result) => {
-        console.log("Successeful login: ", result)
-        tokenStore.setTokens(result)
-        router.push('/me')
-      },
-      onFailure: (error) => {
-        console.log("Failed to log in: ", error)
-      }
-    })
+const login = () => {
+  const authenticationData = {
+    Username: username.value,
+    Password: password.value
   }
+
+  let authenticationDetails = new AuthenticationDetails(authenticationData)
+
+  const userPool = new CognitoUserPool(poolData)
+
+  const userData = {
+    Username: username.value,
+    Pool: userPool
+  }
+
+  const cognitoUser = new CognitoUser(userData)
+
+  cognitoUser.authenticateUser(authenticationDetails, {
+    onSuccess: (result) => {
+      console.log("Successeful login: ", result)
+      tokenStore.setTokens(result)
+      router.push('/me')
+    },
+    onFailure: (error) => {
+      console.log("Failed to log in: ", error)
+    }
+  })
+}
 </script>
