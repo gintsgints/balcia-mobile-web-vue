7928844e7752dd8089ff4f2432bfd8008153f037
Gints Polis <polis.gints@gmail.com>
2023-02-19 19:19:57 +0200

    one more publish to netlify

--- vite.config.ts
+++ vite.config.ts
@@ -8,7 +8,6 @@ import { fileURLToPath, URL } from 'node:url'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/balcia-mobile-web-vue/',
   plugins: [
     vue({
       template: { transformAssetUrls }
